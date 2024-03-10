e602f0ea1ef06ee5d8b17b60104ef53a7bc4ea56
Spandan Barve <spandan567@gmail.com>
2024-03-10 16:48:01 +0530

    bufdix - DataForm

--- app/src/common/DataForm.tsx
+++ app/src/common/DataForm.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 export default function DataForm(
-  props: React.DetailedHTMLProps<
-    React.FormHTMLAttributes<HTMLFormElement>,
-    HTMLFormElement
-  >
+  props: Omit<
+    React.DetailedHTMLProps<
+      React.FormHTMLAttributes<HTMLFormElement>,
+      HTMLFormElement
+    >,
+    "onChange" | "onSubmit"
+  > & {
+    onChange?: (data: Record<string, string>) => void;
+    onSubmit?: (data: Record<string, string>) => void;
+  },
 ) {
   const { onSubmit, onChange, children, ...otherProps } = props;
 
-  function submitHandler(event: React.FormEvent<HTMLFormElement>) {
+  function dataEvent(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
 
@@ -18,24 +24,12 @@ export default function DataForm(
       res[entry[0]] = entry[1] as string;
     });
 
-    onSubmit && onSubmit(event);
-  }
-
-  function changeHandler(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-
-    let res: Record<string, string> = {};
-
-    [...data.entries()].forEach((entry) => {
-      res[entry[0]] = entry[1] as string;
-    });
-
-    onChange && onChange(event);
+    onSubmit && onSubmit(res);
+    onChange && onChange(res);
   }
 
   return (
-    <form onSubmit={submitHandler} onChange={changeHandler} {...otherProps}>
+    <form onSubmit={dataEvent} onChange={dataEvent} {...otherProps}>
       {children}
     </form>
   );

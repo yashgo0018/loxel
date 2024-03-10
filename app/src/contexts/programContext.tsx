24041f026f49a6a160d0d4abfa4b32b6a68e96bc
Yash Goyal <yashgo0018@gmail.com>
2024-03-10 16:02:38 +0000

    updated the program context

--- app/src/contexts/programContext.tsx
+++ app/src/contexts/programContext.tsx
@@ -1,11 +1,14 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import * as anchor from "@coral-xyz/anchor";
 import { useWeb3Context } from "./web3Context";
+import LoxelIdl from "../idl/loxel.json";
 
-interface IProgramContext {}
+interface IProgramContext {
+  program: anchor.Program<anchor.Idl> | null;
+}
 
 export const ProgramContext = createContext<IProgramContext>(
-  {} as IProgramContext
+  {} as IProgramContext,
 );
 
 export default function ProgramContextProvider({
@@ -14,13 +17,27 @@ export default function ProgramContextProvider({
   children: React.ReactNode;
 }) {
   const { providerInitialized } = useWeb3Context();
+  const [program, setProgram] = useState<anchor.Program<anchor.Idl> | null>(
+    null,
+  );
 
   useEffect(() => {
     if (!providerInitialized) return;
+
+    const provider = anchor.getProvider();
+
+    const program = new anchor.Program(
+      LoxelIdl as anchor.Idl,
+      LoxelIdl.metadata.address,
+      provider,
+    );
+    setProgram(program);
   }, [providerInitialized]);
 
   return (
-    <ProgramContext.Provider value={{}}>{children}</ProgramContext.Provider>
+    <ProgramContext.Provider value={{ program }}>
+      {children}
+    </ProgramContext.Provider>
   );
 }
 

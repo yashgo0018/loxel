8ad6cfe881c22e34cd2d94edc6f39dc395176132
Spandan Barve <spandan567@gmail.com>
2024-03-10 17:56:29 +0530

    Fix pass fontsize

--- app/src/common/Pass.tsx
+++ app/src/common/Pass.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { PassData } from "../types";
 import { twMerge } from "tailwind-merge";
 import FormattedDate from "./FormattedDate";
 import { TextureType, getTextureImage } from "../config/textures";
 import TextureOverlay from "./TextureOverlay";
+import { cardAspectRatio } from "../config";
 
 interface PassProps {
   className?: string;
@@ -18,8 +19,23 @@ const embossStyle = {
 export default function Pass(props: PassProps) {
   const { data } = props;
 
+  const [width, setWidth] = useState(0);
+
+  const containerRef = useRef() as React.MutableRefObject<HTMLElement>;
+
+  function getFontSize(x: number) {
+    return width * (x / 100);
+  }
+
+  useEffect(() => {
+    if (width == 0) {
+      setWidth(containerRef.current.getBoundingClientRect().width);
+    }
+  }, []);
+
   return (
     <figure
+      ref={containerRef}
       style={
         {
           "--A": data.colors.primary,
@@ -31,21 +47,24 @@ export default function Pass(props: PassProps) {
     >
       <div
         className={twMerge(
-          "relative rounded-lg aspect-[1.586] cursor-default select-none overflow-hidden bg-A font-dots",
+          "relative rounded-lg cursor-default select-none overflow-hidden bg-A font-dots border border-front/10",
           props.className,
         )}
+        style={{ height: width / cardAspectRatio }}
       >
         <div className="absolute-cover z-30">
           <TextureOverlay type={data.textures.primary} />
 
-          <div className="h-1/3 absolute bottom-0 w-full tracking-tight font-geist">
+          <div className="h-1/3 absolute bottom-0 w-full tracking-tight font-geist text-sm" style={{
+            fontSize : getFontSize(3.6)
+          }}>
             <figure className="absolute-cover bg-B" />
             <TextureOverlay type={data.textures.secondary} />
-            <p className="absolute top-1/2 -translate-y-1/2 right-5 text-D font-medium text-center flex gap-x-1 text-sm">
+            <p className="absolute top-1/2 -translate-y-1/2 right-5 text-D font-medium text-center flex gap-x-1">
               Expires : <FormattedDate timestamp={data.expiry} />
             </p>
 
-            <p className="absolute top-1/2 -translate-y-1/2 left-5 text-D font-medium text-center flex gap-x-1 text-sm">
+            <p className="absolute top-1/2 -translate-y-1/2 left-5 text-D font-medium text-center flex gap-x-1">
               Usages left : {data.usage.used} / {data.usage.total}
             </p>
           </div>
@@ -67,7 +86,9 @@ export default function Pass(props: PassProps) {
             <div className="text-C">
               <h2
                 className="text-xl font-extralight tracking-wider uppercase"
-                style={{ ...embossStyle }}
+                style={{ ...embossStyle, 
+                  fontSize: getFontSize(5.5)
+                 }}
               >
                 {data.passName}
               </h2>
@@ -78,7 +99,9 @@ export default function Pass(props: PassProps) {
         <div className="absolute w-full top-1/3 bottom-1/3 flex justify-center items-center text-C z-30">
           <p
             className="text-2xl font-extralight tracking-wider text-front"
-            style={{ ...embossStyle }}
+            style={{ ...embossStyle, 
+              fontSize : getFontSize(6.4)
+             }}
           >
             {data.userName}
           </p>

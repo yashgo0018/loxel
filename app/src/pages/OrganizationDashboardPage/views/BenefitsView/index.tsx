16f85ac5865ea78628cf4e14555a8b0f1ba37d3c
Riya Jain <riya1jain567@gmail.com>
2024-03-10 22:06:11 +0530

    Benefits page dummy

--- app/src/pages/OrganizationDashboardPage/views/BenefitsView/index.tsx
+++ app/src/pages/OrganizationDashboardPage/views/BenefitsView/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Icon from "../../../../common/Icon";
 import ViewDetails from "../../components/ViewDetails";
+import Coupon from "../../../../common/Coupon";
 
 export default function benefitsView() {
   // function createPassAction() {
@@ -9,6 +10,11 @@ export default function benefitsView() {
   return (
     <article className="py-6 flex flex-col gap-y-6">
       <ViewDetails data={data} className="flex justify-between w-full" />
+      <div className="flex h-full flex-wrap justify-between gap-y-10">
+        {dummyPasses.map((passData, key) => (
+          <Coupon data={passData} key={key} className="w-[30vw]" />
+        ))}
+      </div>
     </article>
   );
 }
@@ -29,3 +35,53 @@ const data = {
     </>
   ),
 };
+
+
+const dummyPasses = [
+  {
+    passName: "Youtube Kids",
+    userName: "Spandan Barve",
+    colors: {
+      primary: "#6c736d",
+      secondary: "#0d751d",
+      text: { primary: "#fefefe", secondary: "#fefefe" },
+    },
+    textures: { primary: "feather", secondary: "glass" },
+    logo: {
+      url: "https://i.pinimg.com/originals/41/e8/20/41e82046cd52130a111049ea560cfbda.png",
+    },
+    expiry: 1834066743000,
+    usage: { total: 10, used: 2 },
+  },
+  {
+    passName: "Youtube Advanced",
+    userName: "Riya Jain",
+    colors: {
+      primary: "#101010",
+      secondary: "#b00b1f",
+      text: { primary: "#ffffff", secondary: "#fefefe" },
+    },
+    textures: { primary: "america", secondary: "glass" },
+    logo: {
+      url: "https://t3.ftcdn.net/jpg/04/74/05/94/360_F_474059464_qldYuzxaUWEwNTtYBJ44VN89ARuFktHW.jpg",
+    },
+    expiry: 1830066943000,
+    usage: { total: 10, used: 6 },
+  },
+
+  {
+    passName: "5% off",
+    userName: "min purchase $500",
+    colors: {
+      primary: "#0d0d0d",
+      secondary: "#a4861f",
+      text: { primary: "#ffffff", secondary: "#000000" },
+    },
+    textures: { primary: "metal", secondary: "hyper" },
+    logo: {
+      url: "https://styles.redditmedia.com/t5_ild9e/styles/communityIcon_8ikf8h653cac1.png",
+    },
+    expiry: 1930066943000,
+    usage: { total: 10, used: 5 },
+  },
+] as const
